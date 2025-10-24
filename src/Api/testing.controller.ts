83440e35512e6afffaa4899299fdc3029820e633
Chaslen2101 <Chaslen2101@gmail.com>
2025-10-25 00:51:51 +0300

    HomeworkProject_13-06

--- src/Api/testing.controller.ts
+++ src/Api/testing.controller.ts
@@ -1,11 +1,28 @@
 import { Controller, Delete } from '@nestjs/common';
-import mongoose from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Blog } from '../Domain/blog.schema';
+import { User } from '../Domain/user.schema';
+import { Post } from '../Domain/post.schema';
+import { Comment } from '../Domain/comment.schema';
+import type { BlogModelType } from '../Domain/blog.schema';
+import type { UserModelType } from '../Domain/user.schema';
+import type { PostModelType } from '../Domain/post.schema';
+import type { CommentModelType } from '../Domain/comment.schema';
 
 @Controller('testing')
 export class TestingController {
+  constructor(
+    @InjectModel(User.name) private UserModel: UserModelType,
+    @InjectModel(Blog.name) private BlogModel: BlogModelType,
+    @InjectModel(Post.name) private PostModel: PostModelType,
+    @InjectModel(Comment.name) private CommentModel: CommentModelType,
+  ) {}
   @Delete('all-data')
   async deleteAllData(): Promise<void> {
-    await mongoose.connection.dropDatabase();
+    await this.BlogModel.deleteMany();
+    await this.PostModel.deleteMany();
+    await this.UserModel.deleteMany();
+    await this.CommentModel.deleteMany();
     return;
   }
 }
